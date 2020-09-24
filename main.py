9becc85e9b62cced6424ecbb8f618ee9614eb0b9
Cherkesky <guy@cherkesky.com>
2020-09-23 22:28:50 -0500

    support for button and enter key together

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import tkinter as tk
+from tkinter import messagebox
 from get_pid import get_pid
 
-def run_get_pid(event):
+def run_get_pid_enter(event):
     pid.configure(state='normal')
     pid.delete("0",tk.END)
     resolved_pid = get_pid('main', name.get())
     pid.insert('end', str(resolved_pid))
+    if resolved_pid==None:
+        messagebox.showwarning("Oops", "No Business Or Multiple Businesses Found.  Try Adding A Location Or Check Spelling")
+
+def run_get_pid():
+    pid.configure(state='normal')
+    pid.delete("0",tk.END)
+    resolved_pid = get_pid('main', name.get())
+    pid.insert('end', str(resolved_pid))
+    if resolved_pid==None:
+        messagebox.showwarning("Oops", "No Business Or Multiple Businesses Found.  Try Adding A Location Or Check Spelling")
 
 root = tk.Tk()
 root.winfo_toplevel().title("GetPID")
 tk.Label(root, text="Enter Business Name").grid(row=0)
 tk.Label(root, text="Place ID (PID) Result:").grid(row=1)
-root.bind('<Return>', run_get_pid)
+root.bind('<Return>', run_get_pid_enter)
 
 name = tk.Entry(root, width=25)
 pid = tk.Entry(root, width=25)
